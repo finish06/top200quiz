3703b80d8e07bdc34d388a611093310afc9b1c91
Caleb Dunn <finish.06@gmail.com>
2021-07-19 07:42:50 -0500

    improve mobile response

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
-import logo from "../assets/medication.png";
+import logo from '../assets/medication.png';
 
 const Header = () => (
-    <header className="row">
-        <div className="col-md-4">
-            <img src={logo} className="logo float-sm-left float-md-end" alt="logo" />
-        </div>
-        <p className="col-md-7 mt-5 subtitle">
-            Test your knowledge of the top 200 medications
-        </p>
-    </header>
+	<header className="row">
+		<div className="col-4">
+			<img src={logo} className="logo float-sm-left float-md-end" alt="logo" />
+		</div>
+		<p className="col-7 mt-5 subtitle">Test your knowledge of the top 200 medications</p>
+	</header>
 );
 
 export default Header;
