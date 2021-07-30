53b9ffd8a201f896ea6ee1e12fee5981f46643af
Caleb Dunn <finish.06@gmail.com>
2021-07-30 04:56:14 -0500

    add footer with credits

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => (
+	<footer id="footer" className="container-fluid">
+		<p className="my-3 text-center">
+			Icons made by <a href="https://www.flaticon.com/authors/freepik">Freepik</a> from{' '}
+			<a href="https://www.flaticon.com/">www.flaticon.com</a>.
+		</p>
+	</footer>
+);
+
+export default Footer;
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from '../components/Header';
 import Question from '../components/Question';
+import Footer from '../components/Footer';
 
 const MainLayout = (props) => (
-    <div className='container-sm'>
-        <Header />
-        <Question drugs={props.drugs}/>
-    </div>
-)
+	<div className="container-sm">
+		<Header />
+		<Question drugs={props.drugs} />
+		<Footer />
+	</div>
+);
 
 export default MainLayout;
